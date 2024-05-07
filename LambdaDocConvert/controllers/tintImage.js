afca2c0c0007e5f4c2085bb847966a9376b285bb
moheet333 <moheetshendarkar@gmail.com>
2024-05-07 10:29:22 +0530

    LambdaImageConvert V1 complete

---
+++ LambdaDocConvert/controllers/tintImage.js
@@ -0,0 +1,19 @@
+const sharp = require("sharp");
+
+const tintImage = async (req, res, next) => {
+  try {
+    const { image, color, type } = req.body;
+    if (!image || !type || !color) {
+      return res.status(400).send("Please provide an image, type and color!");
+    }
+    const imageBuffer = Buffer.from(image, "base64");
+    const tintedImage = await sharp(imageBuffer).tint(color).toBuffer();
+    req.finalImage = tintedImage;
+    req.type = type;
+    next();
+  } catch (error) {
+    res.status(500).send("Error in Tinting: " + error);
+  }
+};
+
+module.exports = tintImage;
--- LambdaDocConvert/routes/convert.js
+++ LambdaDocConvert/routes/convert.js
@@ -5,10 +5,16 @@ const uploadToS3 = require("../middleware/uploadToS3.js");
 const negateImage = require("../controllers/negateImage.js");
 const blurImage = require("../controllers/blurImage.js");
 const grayscaleImage = require("../controllers/grayscaleImage.js");
+const tintedImage = require("../controllers/tintImage.js");
+const sharpedImage = require("../controllers/sharpenImage.js");
+const resizeImage = require("../controllers/resizeImage.js");
 
 router.route("/convert").post(convertType, uploadToS3);
 router.route("/negate").post(negateImage, uploadToS3);
 router.route("/blur").post(blurImage, uploadToS3);
 router.route("/grayscale").post(grayscaleImage, uploadToS3);
+router.route("/tint").post(tintedImage, uploadToS3);
+router.route("/sharpen").post(sharpedImage, uploadToS3);
+router.route("/resize").post(resizeImage, uploadToS3);
 
 module.exports = router;
